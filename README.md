1c2c6d246bbd68b09f24a43785ad8ee7d9f11dd5
Shayne Linhart <shayne.linhart@trackvia.com>
2016-09-08 09:45:33 -0600

    Created initial version of SDK.

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
 # API-Node-SDK
-Node.js SDK for TrackVia
+Node.js SDK for working with application data in TrackVia
+
+## Getting Started
+Login to your Trackvia account and navigate to https://go.trackvia.com/#/my-info. Copy down the API key.
+
+Include the `build/trackvia-api.js` into your project.
+
+Create an instance of the Trackvia api with your API key:
+```javascript
+var TrackviaAPI = require('./path/to/trackvia-api.js');
+
+var api = TrackviaAPI('YOUR KEY HERE');
+```
+
+## Authenticating
+You must authenticate before accessing any data in Trackvia.
+```javascript
+api.login('myTrackviaAccount@gmail.com', 'myPassword')
+.then(() => {
+    // Successfully authenticated..
+    // Make additional request in here
+});
+```
---
+++ src/lib/tv-request.js
@@ -0,0 +1,95 @@
+var request = require('request');
+
+// Uncomment to debug request:
+// require('request-debug')(request);
+
+var auth = require('./auth.js');
+
+global.__tv_host = 'https://go.trackvia.com:443';
+
+var getDefaultOptions = () => {
+    return {
+        requiresAuth: true,
+        form: false,
+        querystring: false
+    };
+};
+
+class tvRequest {
+    makeRequest(requestDetails, options) {
+        var options = Object.assign(getDefaultOptions(), options);
+
+        if(options.form) {
+            requestDetails.form = requestDetails.body;
+
+            if(requestDetails.json && requestDetails.body) {
+                delete requestDetails.json;
+                delete requestDetails.body;
+            }
+        }
+
+
+        if(options.requiresAuth) {
+            var paramType;
+            if(options.querystring || requestDetails.method === 'GET') {
+                paramType = 'qs';
+            } else {
+                paramType = 'body';
+            }
+
+            requestDetails[paramType] = requestDetails[paramType] || {};
+            requestDetails[paramType].access_token = auth.getAccessToken();
+            requestDetails[paramType].user_key = auth.getUserKey();
+        }
+        
+
+        return new Promise((resolve, reject) => {
+            request(requestDetails, (err, res, body) => {
+
+                if(isErrorResponse(res.statusCode)) {
+                    reject(res.statusCode)
+                }
+
+                if(typeof body === 'object' || options.raw) {
+                    resolve(body);
+                } else if(typeof body === 'string') {
+                    resolve(JSON.parse(body));
+                } else {
+                    resolve('');
+                }
+            });
+        });
+    }
+
+    get(url, params, options) {
+        var requestDetails = {
+            url: __tv_host + url,
+            method: 'GET',
+            qs: params
+        };
+
+        return this.makeRequest(requestDetails, options);
+    }
+
+    post(url, params, options) {
+        var options = options || {};
+
+        var requestDetails = {
+            url: __tv_host + url,
+            method: 'POST',
+            json: true
+        };
+        requestDetails[options.querystring ? 'qs' : 'body'] = params;
+
+        return this.makeRequest(requestDetails, options);
+    }
+}
+
+function isErrorResponse(statusCode) {
+    if(statusCode === 0 || statusCode >= 400) {
+        return true;
+    }
+    return false;
+}
+
+module.exports = new tvRequest();

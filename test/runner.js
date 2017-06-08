a4ee07fc27972388ebc8a4888eb29d4476fdda83
Michael Scherer <michaelscherer@Michaels-MacBook-Pro.local>
2017-06-08 16:49:35 -0600

    Ignore test folder

--- test/runner.js
+++ test/runner.js
@@ -5,7 +5,7 @@ var tvApi = new TrackviaAPI('06e44182a2304049fa8fab34251d8db5', 'https://go.trac
 function testIt() {
   tvApi.login('michael.c.scherer@gmail.com', 'WaterBottleEngine1')
   .then(() => {
-    tvApi.attachFile(25, 2, 'This thing', '~/Downloads/6-1-2017 11.05.21pm_template_example (2).docx')
+    tvApi.deleteFile(25, 2, 'This thing')
     .then((returned) =>{
       return console.log(`success: ${JSON.stringify(returned)}`)
     })

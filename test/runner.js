0a518c91a474549d1a335efba1ae16c7c2f8c747
Michael Scherer <michaelscherer@Michaels-MacBook-Pro.local>
2017-06-12 15:48:30 -0600

    Change runner file

--- test/runner.js
+++ test/runner.js
@@ -1,11 +1,11 @@
 var TrackviaAPI = require('../src/trackvia-api');
 
-var tvApi = new TrackviaAPI('06e44182a2304049fa8fab34251d8db5', 'https://go.trackvia.com')
+var tvApi = new TrackviaAPI(/*key, url*/)
 
 function testIt() {
-  tvApi.login('michael.c.scherer@gmail.com', 'WaterBottleEngine1')
+  tvApi.login(/*username, password*/)
   .then(() => {
-    tvApi.deleteFile(25, 2, 'This thing')
+    tvApi.getAppByName()
     .then((returned) =>{
       return console.log(`success: ${JSON.stringify(returned)}`)
     })

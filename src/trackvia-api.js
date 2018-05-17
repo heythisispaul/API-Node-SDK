d13a6ef924dfcb3d7cc5c65039eeebb45dc89911
Katie Scruggs <katie.scruggs@trackvia.com>
2018-05-17 08:42:02 -0600

    Add note that private API endpoint for batch update is not officially supported

--- src/trackvia-api.js
+++ src/trackvia-api.js
@@ -224,7 +224,7 @@ class TrackviaAPI {
     }
 
     /**
-     * Batch update records
+     * Batch update records - not officially supported
      * @param {Number} accountId
      * @param {Number} appId
      * @param {Number} tableId
